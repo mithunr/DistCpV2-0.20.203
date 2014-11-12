d0af6a6d6e68f864a9ec0c80b9ca010dd28e8092
Raju Bairishetti <raju.bairishetti@C1MNG0PXDTY3.local>
2014-11-12 09:27:35 +0530

    calculate the bandwidth in fraction

--- src/main/java/org/apache/hadoop/tools/util/ThrottledInputStream.java
+++ src/main/java/org/apache/hadoop/tools/util/ThrottledInputStream.java
@@ -27,6 +27,7 @@ public class ThrottledInputStream extends InputStream {
   private final long maxBytesPerSec;
   private final long startTime = System.currentTimeMillis();
   private static final long MILLISECONDS_IN_SEC = 1000;
+  private static final float FLOAT_CONSTANT = 1.0f;
 
   private long bytesRead = 0;
   private long totalSleepTime = 0;
@@ -88,12 +89,12 @@ public class ThrottledInputStream extends InputStream {
     return bytesRead;
   }
 
-  public long getBytesPerSec() {
+  public float getBytesPerSec() {
     long elapsedTimeInMilliSecs = System.currentTimeMillis() - startTime;
     if (elapsedTimeInMilliSecs <= MILLISECONDS_IN_SEC) {
-      return bytesRead;
+      return (bytesRead * FLOAT_CONSTANT);
     } else {
-      return (bytesRead * MILLISECONDS_IN_SEC)/ elapsedTimeInMilliSecs;
+      return (bytesRead * MILLISECONDS_IN_SEC * FLOAT_CONSTANT)/ elapsedTimeInMilliSecs;
     }
   }
 
